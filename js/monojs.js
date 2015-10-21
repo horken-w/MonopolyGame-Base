fc2af9f1fc934ce2ed17f9e515390992275351d4
horkenW <horkenwong@gmail.com>
2015-10-21 18:58:50 +0800

    20151021 monopoly
    programming ......

--- js/monojs.js
+++ js/monojs.js
@@ -1,4 +1,4 @@
-var x=70, y=0, $div=$('<div/>'),table=$('.map_table'), map=[7];
+var x=70, y=0, $div=$('<div/>'),table=$('.map_table'), map={"position":['top','left','bottom','right','top','right','bottom','right','top','right','bottom','left','bottom'],'spaces':[5,12,6,2,4,2,4,2,4,4,2,1,2]};
 
 var Mapset=function(e){
 	this.space=e;
@@ -6,31 +6,64 @@ var Mapset=function(e){
 Mapset.prototype={
 	topLine:function(e){
 		for(var i=0; i<e; i++){
-			$div.clone().css({
-				width: 70,
-				height: 70,
-				position: 'absolute',
+			$div.clone().addClass('map_grid').css({
 				bottom: x,
 				right: y
 			}).appendTo(table);
 			x+=70;
 		}
 	},
+	rightLine:function(e){
+		for(var i=0; i<e; i++){
+			$div.clone().addClass('map_grid').css({
+				bottom: x,
+				right: y
+			}).appendTo(table);
+			y-=70;
+		}
+	},
+	bottomLine:function(e){
+		for(var i=0; i<e; i++){
+			$div.clone().addClass('map_grid').css({
+				bottom: x,
+				right: y
+			}).appendTo(table);
+			x-=70;
+		}
+	},
 	leftLine:function(e){
-
-	}
-	
+		for(var i=0; i<e; i++){
+			$div.clone().addClass('map_grid').css({
+				bottom: x,
+				right: y
+			}).appendTo(table);
+			y+=70;
+		}
+	}	
 }
-Mapset.renderMap=function(space){
+Mapset.renderMap=function(position,space){
 	var map=new Mapset();
-	map.topLine(space);
+	switch(position){
+		case 'top':
+			map.topLine(space);
+			break;
+		case 'right':
+			map.rightLine(space);
+			break;
+		case 'bottom':
+			map.bottomLine(space);
+			break;
+		case 'left':
+			map.leftLine(space);
+			break;
+		default:
+			alert('Data error');
+	}
 }
 
 $(function(){
-	for(var i=0; i< map.length; i++){
-		Mapset.renderMap(map[i]);
+	for(var i=0; i< map.position.length; i++){
+		Mapset.renderMap(map.position[i], map.spaces[i]);
 	}
 	
 })
-
-,12,6,1,5,1,5,1,5,3,3,2
